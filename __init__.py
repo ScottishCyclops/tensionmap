d0f99a4bb74c2b67120819967dba82f450852ed0
Scott Winkelmann <scottlandart@gmail.com>
2017-06-16 16:38:06 +0200

    updated copyright + minor code refactor

--- __init__.py
+++ __init__.py
@@ -1,27 +1,26 @@
-#====================== BEGIN GPL LICENSE BLOCK ======================
+#    Tension Map Script is a Blender add-on
+#    Copyright (C) 2017 Scott Winkelmann
+#    Copyright (C) 2014 Jean-Francois Gallant
 #
-#  This program is free software; you can redistribute it and/or
-#  modify it under the terms of the GNU General Public License
-#  as published by the Free Software Foundation; either version 2
-#  of the License, or (at your option) any later version.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
 #
-#  You should have received a copy of the GNU General Public License
-#  along with this program; if not, write to the Free Software Foundation,
-#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-#
-#======================= END GPL LICENSE BLOCK ========================
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import bpy
 
 bl_info = {
-    "name":        "Tension Map script",
+    "name":        "Tension Map Script",
     "author":      "Scott Winkelmann <scottlandart@gmail.com>, Jean-Francois Gallant (PyroEvil)",
-    "version":     (1, 0, 0),
+    "version":     (1, 0, 1),
     "blender":     (2, 78, 0),
     "location":    "Properties Panel > Data Tab",
     "description": "This add-on adds stretch and squeeze information to desired meshes",
@@ -37,6 +36,34 @@ accepted_modifiers = ["ARMATURE", "MESH_CACHE", "CAST", "CURVE", "HOOK", "LAPLAC
                       "SOFT_BODY"]
 
 
+def get_or_create_vertex_group(obj, group_name):
+    """
+    Creates a new vertex group and initializes it only if it doesn't exist then returns it's index
+    :param obj: the object to operate on
+    :param group_name: the name of the group to get or create
+    :return: the index of the vertex group
+    """
+
+    if group_name not in obj.vertex_groups:
+        obj.vertex_groups.new(name=group_name)
+        for i in range(len(obj.data.vertices)):
+            obj.vertex_groups[group_name].add([i], 0.0, "REPLACE")
+    return obj.vertex_groups[group_name].index
+
+
+def get_or_create_vertex_colors(obj, colors_name):
+    """
+    Creates new vertex colors data only if it doesn't exist then returns it's name
+    :param obj: the object to operate on
+    :param colors_name: the name of the colors data to get or create
+    :return: the name of the vertex colors
+    """
+
+    if colors_name not in obj.data.vertex_colors:
+        obj.data.vertex_colors.new(name=colors_name)
+    return obj.data.vertex_colors[colors_name].name
+
+
 def tm_update(obj, scene):
     """
     Updates the tension map for the given object
@@ -49,44 +76,31 @@ def tm_update(obj, scene):
         if obj.data.tm_active:
             global accepted_modifiers
 
-            #check vertex groups and vertex colors existence, add them otherwise
-            if "tm_stretch" not in obj.vertex_groups:
-                obj.vertex_groups.new(name="tm_stretch")
-                for i in range(len(obj.data.vertices)):
-                    obj.vertex_groups["tm_stretch"].add([i], 0.0, "REPLACE")
+            # check vertex groups and vertex colors existence, add them otherwise
+            index_squeeze = get_or_create_vertex_group(obj, "tm_stretch")
+            index_stretch = get_or_create_vertex_group(obj, "tm_squeeze")
+            index_tension = get_or_create_vertex_colors(obj, "tm_tension")
 
-            if "tm_squeeze" not in obj.vertex_groups:
-                obj.vertex_groups.new(name="tm_squeeze")
-                for i in range(len(obj.data.vertices)):
-                    obj.vertex_groups["tm_squeeze"].add([i], 0.0, "REPLACE")
-
-            if "tm_tension" not in obj.data.vertex_colors:
-                obj.data.vertex_colors.new(name="tm_tension")
-
-            index_squeeze = obj.vertex_groups["tm_squeeze"].index
-            index_stretch = obj.vertex_groups["tm_stretch"].index
-            index_tension = obj.data.vertex_colors["tm_tension"].name
-
-            #save modifier viewport show state
-            #temporarly hide modifiers to copy mesh data
+            # save modifier viewport show state
+            # temporarily hide modifiers to copy mesh data
             show_original_state = [False] * len(obj.modifiers)
             for i in range(len(obj.modifiers)):
                 show_original_state[i] = obj.modifiers[i].show_viewport
 
-                #TODO: clear the next statement out
+                # TODO: clear the next statement out
                 if obj.modifiers[i].type not in accepted_modifiers:
                     obj.modifiers[i].show_viewport = False
 
             mesh = obj.to_mesh(scene=scene, apply_modifiers=True, settings="PREVIEW")
 
-            #restore modifiers viewport show state
+            # restore modifiers viewport show state
             for i in range(len(obj.modifiers)):
                 obj.modifiers[i].show_viewport = show_original_state[i]
 
-            #array to store new weight for each vertices
+            # array to store new weight for each vertices
             weights = [0.0] * len(obj.data.vertices)
 
-            #calculating the new weights
+            # calculating the new weights
             for edge in obj.data.edges:
                 first_vertex = edge.vertices[0]
                 second_vertex = edge.vertices[1]
@@ -98,12 +112,12 @@ def tm_update(obj, scene):
                 weights[first_vertex] -= factor
                 weights[second_vertex] -= factor
 
-            #delete the temporary non-deformed mesh
+            # delete the temporary non-deformed mesh
             bpy.data.meshes.remove(mesh)
 
-            #we put the new values in the vertex groups
-            #if the weight value is positive, it is stretched
-            #otherwise, it is squeezed
+            # we put the new values in the vertex groups
+            # if the weight value is positive, it is stretched
+            # otherwise, it is squeezed
             for i in range(len(obj.data.vertices)):
                 if weights[i] >= 0:
                     obj.vertex_groups[index_stretch].add([i], weights[i], "REPLACE")
@@ -112,12 +126,12 @@ def tm_update(obj, scene):
                     obj.vertex_groups[index_stretch].add([i], 0.0, "REPLACE")
                     obj.vertex_groups[index_squeeze].add([i], -weights[i], "REPLACE")
 
-            #we put the new values in the vertex colors
-            #red channel is stretched
-            #green channel is squeezed
-            #blue channel is not used
+            # we put the new values in the vertex colors
+            # red channel is stretched
+            # green channel is squeezed
+            # blue channel is not used
             for i in range(len(obj.data.polygons)):
-                #k v -> key value
+                # k v -> key value
                 for k, v in enumerate(obj.data.polygons[i].loop_indices):
                     vertex = obj.data.polygons[i].vertices[k]
                     obj.data.vertex_colors[index_tension].data[v].color.r = \
@@ -127,16 +141,17 @@ def tm_update(obj, scene):
                     obj.data.vertex_colors[index_tension].data[v].color.b = 0.0
 
 
-def tm_update_all(scene):
+def tm_update_handler(scene):
     """
     Updates the tension map for all objects in the scene
+    This function will be called by Blender every frame
     :param scene: the scene to operate on
     :return: nothing
     """
 
     global last_processed_frame
 
-    #avoid executing the operations if the frame hasn't really changed
+    # avoid executing the operations if the frame hasn't really changed
     if last_processed_frame != scene.frame_current:
         last_processed_frame = scene.frame_current
         for obj in scene.objects:
@@ -157,9 +172,9 @@ class TmUpdateSelected(bpy.types.Operator):
     Update tension map for selected object
     """
 
-    #this operator is simply a wrapper for the tm_update_selected function
-    bl_idname = "tm.update_selected"
+    # this operator is simply a wrapper for the tm_update_selected function
     bl_label = "Update tension map"
+    bl_idname = "tm.update_selected"
     bl_options = {"REGISTER"}
 
     def execute(self, context):
@@ -220,8 +235,8 @@ def add_handlers():
     Method responsible for adding the handlers for the tm_update_all method
     :return: nothing
     """
-    bpy.app.handlers.persistent(tm_update_all)
-    bpy.app.handlers.frame_change_post.append(tm_update_all)
+    bpy.app.handlers.persistent(tm_update_handler)
+    bpy.app.handlers.frame_change_post.append(tm_update_handler)
 
 
 def remove_handlers():
@@ -229,7 +244,7 @@ def remove_handlers():
     Method responsible for removing the handlers for the tm_update_all method
     :return: nothing
     """
-    bpy.app.handlers.frame_change_post.remove(tm_update_all)
+    bpy.app.handlers.frame_change_post.remove(tm_update_handler)
 
 
 def register():
@@ -254,6 +269,6 @@ def unregister():
     remove_props()
 
 
-#if the script is run directly, register it
+# if the script is run directly, register it
 if __name__ == "__main__":
     register()
